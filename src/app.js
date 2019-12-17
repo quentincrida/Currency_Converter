3b06899e6a2fdc90e34bb3ccce14285bd2c4a03c
Quentin Crida <qcrida@gmail.com>
2019-12-17 21:39:05 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#app",
+
+    // mounted(){
+    // }
+    // getCurrencies: function(){
+    //   fetch("https://api.exchangeratesapi.io/latest")
+    //   .then(res => res.json())
+    //   .then(currencies => this.currencies = currencies)
+    //     }
+  })
+})
